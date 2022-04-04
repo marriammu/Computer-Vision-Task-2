0950f19664c10b0dcae5f38999d63a2681fe60b3
Lola-Tarek <alaa.mohamed99@eng-st.cu.edu.eg>
2022-04-04 15:59:41 +0200

    finaaal

--- snakes_show.py
+++ snakes_show.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import snake
 import numpy as np
+import cv2
 def snakes(image,x,y,z):
     img = snake.Snake( image, closed = True )
     img.set_alpha(x)
@@ -22,3 +23,20 @@ def snakes(image,x,y,z):
     print("area=",area)
     print("............")
     print("perimeter=",perimeter)
+
+def animation(image,x,y,z):
+    img = snake.Snake( image, closed = True )
+    snake_window_name = "Snakes"
+    img.set_alpha(x)
+    img.set_beta(y)
+    img.set_gamma(z)
+    while(True):
+        snakeImg = img.visualize()
+        cv2.imshow( snake_window_name, snakeImg )
+        snake_changed = img.step() 
+    #Stops looping when ESC pressed
+        k = cv2.waitKey(33)
+        if k == 27:
+            break
+
+cv2.destroyAllWindows()
